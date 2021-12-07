253841f3d979ad7d83acd467785047946ef35cda
Kyronectis <76741952+Kyronectis@users.noreply.github.com>
2021-12-07 12:21:14 +0100

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -12,14 +12,10 @@ float JSN_SR04T(PinName trig, PinName echo){
     sonar.reset();
     // measure actual software polling timer delays
     // delay used later in time correction
-    // start timer
-    sonar.start();
-    // min software polling delay to read USecho pin
-    while (USecho==2) {};
-    // stop timer
-    sonar.stop();
-    // read timer
-    correction = sonar.elapsed_time().count();
+    sonar.start(); // start timer
+    while (USecho==2) {}; // min software polling delay to read USecho pin
+    sonar.stop(); // stop timer
+    correction = sonar.elapsed_time().count(); // read timer
     //printf("Approximate software overhead timer delay is %d uS\n\r",correction);
  
     // UStrig sonar to send a ping
@@ -27,17 +23,12 @@ float JSN_SR04T(PinName trig, PinName echo){
     sonar.reset();
     wait_us(20.0);
     UStrig = 0;
-    //wait for echo high
-    while (USecho==0) {};
-    //echo high, so start timer
-    sonar.start();
-    //wait for echo low
-    while (USecho==1) {};
-    //stop timer and read value
-    sonar.stop();
-    //subtract software overhead timer delay and scale to cm
-    float sonar_corrected = (sonar.elapsed_time().count()-correction);
-    sonar_distance = sonar_corrected * 0.0343 / 2; //s of s in w is about 1482 m/s
+    while (USecho==0) {}; //wait for echo high
+    sonar.start(); //echo high, so start timer
+    while (USecho==1) {}; //wait for echo low
+    sonar.stop(); //stop timer and read value
+    float sonar_corrected = (sonar.elapsed_time().count()-correction); //subtract software overhead timer delay and scale to cm
+    sonar_distance = sonar_corrected * 0.0343 / 2; //speed of sound in water is 1482 m/s, currently set at sos in air for testing
     //printf(" %d cm \n\r", sonar_distance);
     //wait so that any echo(s) return before sending another ping
     ThisThread::sleep_for(40ms);
@@ -58,23 +49,19 @@ void LCD164(
     bool power, 
     bool hover, 
     bool control,  
-    PinName RS, 
-    PinName E, 
-    PinName DATA1, 
-    PinName DATA2, 
-    PinName DATA3, 
-    PinName DATA4,
+    TextLCD lcd,
     string message
 ){
-    TextLCD lcd(RS, E, DATA1, DATA2, DATA3, DATA4, TextLCD::LCD16x4);
     lcd.locate(0, 0);
     lcd.printf("PWR %s", power ? "ON " : "OFF");
     lcd.locate(0, 1);
     lcd.printf("HVR %s", hover ? "ON " : "OFF");
     lcd.locate(0, 2);
     lcd.printf("CTL %s", control ? "ON " : "OFF");
+    char msg_c[16];
+    strcpy(msg_c, message.c_str());
     lcd.locate(0, 3);
-    lcd.printf("%s", message.c_str());
+    lcd.printf("%s", msg_c);
     lcd.locate(10, 0);
     lcd.printf("%.2f", distance);
 }
@@ -88,4 +75,9 @@ bool SW_NC(PinName port, bool type){ //Type 0 for regular switch, Type 1 for Mic
     else{
         return state;
     }
+}
+
+int init(BTS7960 hbridge, TextLCD lcd, PinName T, PinName E, PinName SH, PinName GO){
+    
+    return 0;
 }
